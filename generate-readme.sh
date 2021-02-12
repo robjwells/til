19ba39c0dcaa9c5ddda4af444e2baf0f26cab2df
Rob Wells <rob@robjwells.com>
2021-02-12 23:21:27 +0000

    Update readme-generating script

---
+++ generate-readme.sh
@@ -0,0 +1,14 @@
+#!/usr/local/bin/zsh
+
+PATH="/usr/local/bin:$PATH"
+
+cat preamble.md
+
+fd . -t d --exact-depth=1 | while read directory; do
+	echo "\n## $directory\n"
+	git ls-files "$directory" | while read filename; do
+		topic="`rg '.*\d{12} ([^./]+).md' -r '$1' <<< "$filename"`"
+		linksafe="`sd ' ' '%20' <<< "$filename"`"
+		echo "- [$topic]($linksafe)"
+	done
+done
---
+++ preamble.md
@@ -0,0 +1,3 @@
+# Today I learned
+
+My “today I learned” snippets. Inspired by [Simon Willison](https://github.com/simonw/til).
