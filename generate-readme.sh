2272ffc841530718c76eada0188007e01957ef8d
Rob Wells <rob@robjwells.com>
2021-02-12 23:25:08 +0000

    Ensure readme entries are sorted.

--- generate-readme.sh
+++ generate-readme.sh
@@ -4,9 +4,9 @@ PATH="/usr/local/bin:$PATH"
 
 cat preamble.md
 
-fd . -t d --exact-depth=1 | while read directory; do
+fd . -t d --exact-depth=1 | sort | while read directory; do
 	echo "\n## $directory\n"
-	git ls-files "$directory" | while read filename; do
+	git ls-files "$directory" | sort | while read filename; do
 		topic="`rg '.*\d{12} ([^./]+).md' -r '$1' <<< "$filename"`"
 		linksafe="`sd ' ' '%20' <<< "$filename"`"
 		echo "- [$topic]($linksafe)"
