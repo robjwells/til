ed1520f655518899e6d92ac78a620c32176a6458
Rob Wells <rob@robjwells.com>
2021-02-12 23:30:36 +0000

    generate-readme: relax the TIL file regex

--- generate-readme.sh
+++ generate-readme.sh
@@ -7,7 +7,7 @@ cat preamble.md
 fd . -t d --exact-depth=1 | sort | while read directory; do
 	echo "\n## $directory\n"
 	git ls-files "$directory" | sort | while read filename; do
-		topic="`rg '.*\d{12} ([^./]+).md' -r '$1' <<< "$filename"`"
+		topic="`rg '^.*\d{12} (.+)\.md$' -r '$1' <<< "$filename"`"
 		linksafe="`sd ' ' '%20' <<< "$filename"`"
 		echo "- [$topic]($linksafe)"
 	done
